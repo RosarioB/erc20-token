833a61cd9b493b6a0cf30af43387ff64d108887b
Rosario Borgesi <borgesiros@gmail.com>
2023-10-14 16:22:24 +0200

    First commit

--- scripts/deployGLDToken.ts
+++ scripts/deployGLDToken.ts
@@ -8,11 +8,10 @@ const main = async () : Promise<void> => {
 
   const GldToken: GLDToken__factory = await ethers.getContractFactory("GLDToken");
   console.log("Deploying " + CONTRACT_NAME + "...");
-  const gldToken: GLDToken = await GldToken.deploy(TOKEN_CAP, TOKEN_BLOCK_REWARD);
+  const gldToken: GLDToken = await GldToken.deploy();
   await gldToken.waitForDeployment();
   console.log(CONTRACT_NAME + ' deployed to: ', await gldToken.getAddress());
 }
-
 
 main().catch((error) => {
   console.error(error);
